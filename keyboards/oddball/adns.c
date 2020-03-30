c95e15d50caedbf7f8f8969de990a7e4d16065f8
Jup Itzme <qurn@gitlab.com>
2020-03-30 21:54:49 +0200

    make tracking ball on interupt for 1 second very resposive to eliminate jumping

--- keyboards/oddball/adns.c
+++ keyboards/oddball/adns.c
@@ -4,6 +4,7 @@
 #include "pointing_device.h"
 #include "adns9800_srom_A4.h"
 #include "../../lib/lufa/LUFA/Drivers/Peripheral/SPI.h"
+#include "timer.h"
 
 // registers
 #define Product_ID  0x00
@@ -92,54 +93,38 @@ void adns_end(void){
     PORTB |= (1 << NCS);
 }
 
-
-
 void adns_write(uint8_t reg_addr, uint8_t data){
-
     adns_begin();
-
     //send address of the register, with MSBit = 1 to indicate it's a write
     SPI_TransferByte(reg_addr | 0x80 );
     SPI_TransferByte(data);
-
     // tSCLK-NCS for write operation
     wait_us(20);
-
     adns_end();
-
     // tSWW/tSWR (=120us) minus tSCLK-NCS. Could be shortened, but is looks like a safe lower bound
     wait_us(100);
 }
 
 uint8_t adns_read(uint8_t reg_addr){
-
     adns_begin();
-
     // send adress of the register, with MSBit = 0 to indicate it's a read
     SPI_TransferByte(reg_addr & 0x7f );
     uint8_t data = SPI_TransferByte(0);
-
     // tSCLK-NCS for read operation is 120ns
     wait_us(1);
-
     adns_end();
-
     //  tSRW/tSRR (=20us) minus tSCLK-NCS
     wait_us(19);
-
     return data;
 }
 
 void pointing_device_init(void) {
-
     if(!is_keyboard_master())
         return;
-
     // interrupt 2
     EICRA &= ~(1 << 4);
     EICRA |= (1 << 5);
     EIMSK |= (1<<INT2);
-
     // mode 3
     SPI_Init(
         SPI_SPEED_FCPU_DIV_128 |
@@ -147,49 +132,37 @@ void pointing_device_init(void) {
         SPI_SCK_LEAD_FALLING |
         SPI_SAMPLE_TRAILING |
         SPI_MODE_MASTER);
-
     // set B0 output
     DDRB |= (1 << 0);
-
     // reset serial port
     adns_end();
     adns_begin();
     adns_end();
-
     // reboot
     adns_write(Power_Up_Reset, 0x5a);
     wait_ms(150);
-
     // read registers and discard
     adns_read(Motion);
     adns_read(Delta_X_L);
     adns_read(Delta_X_H);
     adns_read(Delta_Y_L);
     adns_read(Delta_Y_H);
-
     // upload firmware
-
     // set the configuration_IV register in 3k firmware mode
     // bit 1 = 1 for 3k mode, other bits are reserved
     adns_write(Config2, 0x20);
-
     // write 0x1d in SROM_enable reg for initializing
     adns_write(SROM_Enable, 0x1d);
-
     // wait for more than one frame period
     // assume that the frame rate is as low as 100fps... even if it should never be that low
     wait_ms(10);
-
     // write 0x18 to SROM_enable to start SROM download
     adns_write(SROM_Enable, 0x18);
-
     // write the SROM file (=firmware data)
     adns_begin();
-
     // write burst destination adress
     SPI_TransferByte(SROM_Load_Burst | 0x80);
     wait_us(15);
-
     // send all bytes of the firmware
     unsigned char c;
     for(int i = 0; i < firmware_length; i++){
@@ -197,18 +170,14 @@ void pointing_device_init(void) {
         SPI_TransferByte(c);
         wait_us(15);
     }
-
     //Read the SROM_ID register to verify the ID before any other register reads or writes.
     adns_read(SROM_ID);
-
     //Write 0x00 to Config2 register for wired mouse or 0x20 for wireless mouse design.
     adns_write(Config2, 0x00);
-
     // set initial CPI resolution
-    adns_write(Config1, 0x15);
-
+    ////adns_write(Config1, 0x15); // was this
+    adns_write(Config1, 0x04);
     adns_end();
-
     wait_ms(10);
 
 //    // enable laser(bit 0 = 0b), in normal mode (bits 3,2,1 = 000b)
@@ -218,16 +187,27 @@ void pointing_device_init(void) {
 //    uint8_t laser_ctrl0 = adns_read(REG_LASER_CTRL0);
 //    adns_write(REG_LASER_CTRL0, laser_ctrl0 & 0xf0);
 
-    wait_ms(1);
+    //wait_ms(1);
 
-    // set the configuration_I register to set the CPI
-    // 0x01 = 50, minimum
-    // 0x44 = 3400, default
-    // 0x8e = 7100
-    // 0xA4 = 8200, maximum
-    adns_write(Config1, 0x04);
+    //// set the configuration_I register to set the CPI
+    //// 0x01 = 50, minimum
+    //// 0x44 = 3400, default
+    //// 0x8e = 7100
+    //// 0xA4 = 8200, maximum
+    //adns_write(Config1, 0x04);
 
-    wait_ms(100);
+    //wait_ms(100);
+}
+
+#include "pointing_device.h"
+
+void on_mouse_button(uint8_t mouse_button, bool pressed) {
+    report_mouse_t report = pointing_device_get_report();
+    if(pressed)
+        report.buttons |= mouse_button;
+    else
+        report.buttons &= ~mouse_button;
+    pointing_device_set_report(report);
 }
 
 void tap_tb(int16_t delta, uint16_t keycode0, uint16_t keycode1) {
@@ -252,17 +232,65 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		else {
 			trackMode = 0; 
 		}
-      } else {
-      }
-      break;
+      } 
+  	  break;
+    case KC_BTN1:
+      on_mouse_button(MOUSE_BTN1, record->event.pressed);
+      return false;
+    case KC_BTN2:
+      on_mouse_button(MOUSE_BTN2, record->event.pressed);
+      return false;
+    case KC_BTN3:
+      on_mouse_button(MOUSE_BTN3, record->event.pressed);
+      return false;
+    case KC_BTN4:
+      on_mouse_button(MOUSE_BTN4, record->event.pressed);
+      return false;
+    case KC_BTN5:
+      on_mouse_button(MOUSE_BTN5, record->event.pressed);
+      return false;
+    default:
+      return true;
   }
   return true;
-};
+}
 
 int32_t cum_x = 0;
 int32_t cum_y = 0;
 
+int8_t xpol = 1;
+int8_t ypol = 1;
+
+int16_t convertDeltaToInt(uint8_t high, uint8_t low){
+    // join bytes into twos compliment
+    uint16_t twos_comp = (high << 8) | low;
+    // convert twos comp to int
+    if (twos_comp & 0x8000)
+        return -1 * ((twos_comp ^ 0xffff) + 1);
+    return twos_comp;
+}
+
+static uint16_t cur_time = 0;
+static uint16_t off_time = 0;
+
 void pointing_device_task(void) {
+    if(!is_keyboard_master()) {
+        return;
+	}
+	cur_time = timer_read();
+    if(cur_time > off_time) {
+        return;
+	}
+    adns_begin();
+    // send adress of the register, with MSBit = 1 to indicate it's a write
+    SPI_TransferByte(Motion_Burst & 0x7f);
+    uint8_t burst_data[pixel_sum];
+    for (int i = 0; i < pixel_sum; ++i) {
+        burst_data[i] = SPI_TransferByte(0);
+    }
+	delta_x = convertDeltaToInt(burst_data[delta_x_h], burst_data[delta_x_l]);
+    delta_y = convertDeltaToInt(burst_data[delta_y_h], burst_data[delta_y_l]);
+    adns_end();
     if(!is_keyboard_master() || (delta_x == 0 && delta_y ==0))
         return;
     report_mouse_t report = pointing_device_get_report();
@@ -288,43 +316,20 @@ void pointing_device_task(void) {
 	} else {
 		cum_x = cum_x + delta_x;
 		cum_y = cum_y + delta_y;
-		if(abs(cum_x) + abs(cum_y) >= 10){
+		if(abs(cum_x) + abs(cum_y) >= 5){
 			if(abs(cum_x) > abs(cum_y)) {
-				report.h = cum_x/10;
+				report.h = cum_x/5;
 			} else {
-				report.v = -cum_y/10;
+				report.v = -cum_y/5;
 			}
 			cum_x = 0;
 			cum_y = 0;
 		}
     }
-    // reset deltas
-    delta_x = 0;
-    delta_y = 0;
     pointing_device_set_report(report);
     pointing_device_send();
 }
 
-int16_t convertDeltaToInt(uint8_t high, uint8_t low){
-    // join bytes into twos compliment
-    uint16_t twos_comp = (high << 8) | low;
-    // convert twos comp to int
-    if (twos_comp & 0x8000)
-        return -1 * ((twos_comp ^ 0xffff) + 1);
-    return twos_comp;
-}
-
 ISR(INT2_vect) {
-    // called on interrupt 2 when sensed motion
-    // copy burst data from the respective registers
-    adns_begin();
-    // send adress of the register, with MSBit = 1 to indicate it's a write
-    SPI_TransferByte(Motion_Burst & 0x7f);
-    uint8_t burst_data[pixel_sum];
-    for (int i = 0; i < pixel_sum; ++i) {
-        burst_data[i] = SPI_TransferByte(0);
-    }
-    delta_x += convertDeltaToInt(burst_data[delta_x_h], burst_data[delta_x_l]);
-    delta_y += convertDeltaToInt(burst_data[delta_y_h], burst_data[delta_y_l]);
-    adns_end();
+	off_time = timer_read() + 1000;
 }
